0ade8a1bf747ced5304b7906a36db894433e5042
ian <ian.mackerracher@uni-konstanz.de>
2017-11-05 13:17:12 +0100

    small changes

---
+++ player.py
@@ -0,0 +1,14 @@
+class Player(Object):
+    
+    def __init__(unique_id, current_session_id, client_ip, nickname):
+        self.unique_id = unique_id
+        self.current_session_id = current_session_id
+        self.score = 0
+        self.client_ip = client_ip
+        slef.nickname = nickname
+        
+        
+    #update the game
+    def change_game_state():
+        pass
+        
