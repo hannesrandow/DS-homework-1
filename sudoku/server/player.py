18ca67e7a7ca6b728c2af1ae934f271534fc1ad7
ian <ian.mackerracher@uni-konstanz.de>
2017-11-14 21:45:39 +0100

    Added some documentation.

--- sudoku/server/player.py
+++ sudoku/server/player.py
@@ -2,7 +2,15 @@ import pickle
 
 
 class Player(object):
+    """
+    When the client joins a session or creates a new one, the server creates a player instance.
+    """
     def __init__(self, client_ip, nickname=None, current_session_id=None):
+        """
+        :param client_ip: IP and port of the client.
+        :param nickname: The nickname that the user gave himself.
+        :param current_session_id: The session that the user is currently in.
+        """
         self.client_ip = client_ip
         self.nickname = nickname
         self.current_session_id = current_session_id
@@ -20,7 +28,11 @@ class Player(object):
         pass
 
     def send_game_updates(self, session):
-        """This method is called for every player when their game udpated"""
+        """
+        This method is called for every player when their game udpated.
+        :param session: The session that the player is currently in.
+        :return: None
+        """
         pickle_session = pickle.dumps(session)
         if self.link_back_sock:
             print ("send update to ", self.nickname)
@@ -29,5 +41,9 @@ class Player(object):
             print("#### link back socket not existing! ####")
 
     def close(self):
+        """
+        This method gets calles when the client disconnects.
+        :return: None
+        """
         if self.link_back_sock:
             self.link_back_sock.close()
