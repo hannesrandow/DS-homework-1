d7646c3b948a38fcb3c72044d8b7645ec7926874
ian <ian.mackerracher@uni-konstanz.de>
2017-12-20 15:36:00 +0100

    Added updates to game via publish/subscribe on server side

---
+++ sudoku/server/ic_server_update.py
@@ -0,0 +1,42 @@
+import pika
+import pickle
+
+"""
+Here the server will send updates to the sudoku game to the clients that are subscribed to the game.
+"""
+
+class ICServerUpdate:
+
+
+    def __init__(self, game_name):
+        self.game_name = game_name
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.game_name,
+                                      exchange_type='fanout')
+        # self.channel.queue_declare(queue=self.name)
+        print('Sudoku game %s running...' % self.game_name)
+        self.users = []
+
+    def add_user(self, username):
+        self.users.append(username)
+        return
+
+    def publish_update(self, update):
+        update = pickle.dumps(update)
+        print('published update')
+        self.channel.basic_publish(exchange=self.game_name,
+                                   routing_key='',
+                                   body=update)
+        if len(self.users) == 0:
+            self.delete_game()
+        return
+
+    def delete_game(self):
+        """
+        TODO: announce winner before game is deleted
+        :return:
+        """
+        self.channel.close()
+        self.connection.close()
+        return
